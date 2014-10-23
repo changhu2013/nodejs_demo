8f613cdc591207ecba2868f01b9196f039cba87f
changhu <o52tiger@gmail.com>
2014-10-23 13:23:06 +0800

    初始导入

---
+++ app.js
@@ -0,0 +1,40 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var url = require('url');
+var path = require('path'); 
+var fs = require('fs');  
+
+app.get('/', function(req, res){
+    res.sendfile('index.html');
+});
+
+//监听当有一个客户端连接上以后处理
+io.on('connection', function(socket){
+
+    console.log('一个用户端连接上 ' + socket);
+
+    socket.on('disconnect', function(){
+        console.log('一个用户断开连接');
+    });
+    
+    socket.on('chat', function(msg){
+        console.log('消息: ' + msg);
+        io.emit('chat', msg);
+    });
+});
+
+//处理静态文件
+app.all('/*.(html|css|js|jpg|png){1}', function(req, res, next){
+    var realpath = __dirname + '/static' + url.parse(req.url).pathname;
+    console.log('realpath:' + realpath);
+    if(path.existsSync(realpath)){
+        res.end(fs.readFileSync(realpath));
+    }else {
+        res.end('Cannot find request url: ' + req.url);
+    }
+});
+
+http.listen(3000, function(){
+    console.log('监听端口:3000');
+});
